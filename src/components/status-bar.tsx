7c7262006be58d8987903f9f2b760531d92e2ddd
Abilash42 <abilash.suresh199@gmail.com>
2025-05-05 22:05:39 -0400

    eslint (derogatory)

--- src/components/status-bar.tsx
+++ src/components/status-bar.tsx
@@ -57,7 +57,7 @@ export default function StatusBar({
 
   const { mutate } = api.typingEntry.add.useMutation({
     onSuccess: () => {
-      utils.typingEntry.getAll.invalidate();
+      void utils.typingEntry.getAll.invalidate();
     },
   });
 
