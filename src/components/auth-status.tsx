e0ca1ec2019d5354c748c89e2dda2499d7b7d8ca
Abilash42 <abilash.suresh199@gmail.com>
2025-05-05 21:26:26 -0400

    Update auth-status.tsx

--- src/components/auth-status.tsx
+++ src/components/auth-status.tsx
@@ -16,7 +16,7 @@ export default function AuthStatus() {
           <button
             className="text-s mr-5 font-mono text-gray-300 transition-colors hover:text-gray-500"
             onClick={async () => {
-              await goToSignIn();
+              goToSignIn();
             }}
           >
             sign in
