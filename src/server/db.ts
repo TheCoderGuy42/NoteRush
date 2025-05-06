c89556cef2e2e5effdd406a3882716a3e4459c79
Abilash42 <abilash.suresh199@gmail.com>
2025-05-06 00:48:10 -0400

    not looking good folks

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { withOptimize } from "@prisma/extension-optimize";
 
 import { env } from "@/env";
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const createPrismaClient = () => new PrismaClient().$extends(withAccelerate());
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
