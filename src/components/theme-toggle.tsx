c197d7024ab0c38f00ec7d1c91a21e4c7d3da38e
Abilash42 <abilash.suresh199@gmail.com>
2025-05-07 18:16:26 -0400

    Update theme-toggle.tsx

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -14,7 +14,7 @@ export function ThemeToggle() {
       aria-label="Toggle theme"
     >
       <Sun className="h-5 w-5 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-5 w-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Moon className="h-5 w-5 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </button>
   );
