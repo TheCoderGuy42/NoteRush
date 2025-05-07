d872e12b694f7ba2150f65465731952f8178e267
Abilash42 <abilash.suresh199@gmail.com>
2025-05-07 13:41:11 -0400

    support button

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -464,7 +464,14 @@ function App() {
             )}
           </>
         )}
-        <HelpButton />
+        <a
+          href="https://x.com/chainsaw_co"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-s cursor-pointer font-mono text-gray-300 transition-colors hover:text-gray-500"
+        >
+          contact
+        </a>
 
         <input
           ref={fileInputRef}
